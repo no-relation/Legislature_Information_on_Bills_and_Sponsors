cc80c2379b6e20097af06dbf906b6994f4552231
github username <github email address>
2018-10-22 23:42:03 -0500

    almost done with seed file, but getting TypeError for line 20

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,8 +1,8 @@
 require_relative 'api_communicator'
 
-fake_dude = Legislator.create(full_name: "Fakey McFaker", leg_id: "FAKEID", party: "Democrat")
-fake_bill = Bill.create(title: "Bill to make things better", openstates_id: "TXB00000000")
-fake_sponsorship = Sponsorship.create(bill: fake_bill, legislator: fake_dude)
+# fake_dude = Legislator.create(full_name: "Fakey McFaker", leg_id: "FAKEID", party: "Democrat")
+# fake_bill = Bill.create(title: "Bill to make things better", openstates_id: "TXB00000000")
+# fake_sponsorship = Sponsorship.create(bill: fake_bill, legislator: fake_dude)
 
 # get_bills: title, openstates_id
 get_bills.each do |bill|
@@ -16,9 +16,9 @@ end
 
 # get_individual_bill(bill_id): sponsorships: bill_id, legislator_id, type
 Bill.all.each do |bill|
-    get_individual_bill(bill[openstates_id]).each do |bill_stats|
+    get_individual_bill(bill[:openstates_id]).each do |bill_stats|
         bill_stats["sponsors"].each do |sponsor|
-            Sponsorship.create(openstates_id: bill_stats["id"], leg_id: sponsor["leg_id"], type: sponsor["type"])
+            Sponsorship.create(bill_id: bill_stats["id"], legislature_id: sponsor["leg_id"], type: sponsor["type"])
         end
     end
 end
