f607bbe09543505614f257d094fbeece7dee2508
ch0mper <hannahchang17@gmail.com>
2018-10-24 18:00:12 -0500

    more methods for bills

--- app/models/bill.rb
+++ app/models/bill.rb
@@ -1,63 +1,14 @@
 class Bill < ActiveRecord::Base
     has_many :sponsorships
     has_many :legislators, through: :sponsorships
-
+    
+    #helper method
     def self.get_ID(query_id)
         self.all.find do |bill|
             bill.openstates_id == query_id
-        end.id
-    end
-
-    def self.most_dem
-        # looks through all bills and counts how many dem sponsors per bill, returns the bill with largest count
-        most_dem_count = 0
-        most_dem_bill = []
-        i = 1
-        while i <= Bill.all.length
-            the_count = Bill.find(i).sponsorships.count do |sponsor|
-                if sponsor["legislator_id"] != 0
-                    Legislator.find(sponsor["legislator_id"]).party == "Democratic"
-                end
-            end
-            if the_count > most_dem_count
-                most_dem_count = the_count
-                most_dem_bil = []
-                most_dem_bill << Bill.find(i)
-            elsif the_count == most_dem_count
-                most_dem_bill << Bill.find(i)
-            end
-            i += 1
-        end
-        titles = most_dem_bill.map {|bill| bill.title}
-        puts "There is/are #{titles.length} bill(s) with the most (#{most_dem_count}) dem sponsors: #{titles}."
-        return most_dem_bill
-    end
-
-    def self.most_repub
-        #same as above with repub
-        most_repub_count = 0
-        most_repub_bill = []
-        i = 1
-        while i <= Bill.all.length
-            the_count = Bill.find(i).sponsorships.count do |sponsor|
-                if sponsor["legislator_id"] != 0
-                    Legislator.find(sponsor["legislator_id"]).party == "Republican"
-                end
-            end
-            if the_count > most_repub_count
-                most_repub_count = the_count
-                most_repub_bill = []
-                most_repub_bill << Bill.find(i)
-            elsif the_count == most_repub_count
-                most_repub_bill << Bill.find(i)
-            end
-            i += 1
-        end
-        titles = most_repub_bill.map {|bill| bill.title}
-        puts "There is/are #{titles.length} bill(s) with the most (#{most_repub_count}) repub sponsors: #{titles}."
-        return most_repub_bill
-    end
-
+        end.id    
+    end    
+    
     #helper method
     def get_bill_id
         self.id
@@ -89,19 +40,108 @@ class Bill < ActiveRecord::Base
             Legislator.find(sponsor["legislator_id"]).full_name
         end
     end
+    
+
+    def self.most_sponsors
+        # output {num of sponsors => [the bill instances]}
+        # use #all_sponsors or #all_sponsors_by_party to return the sponsors of a specific bill
+        most_sponsors_count = 0
+        most_sponsors_bill = []
+        Bill.all.each do |bill|
+            the_count = bill.sponsorships.count
+            if the_count > most_sponsors_count
+                most_sponsors_count = the_count
+                most_sponsors_bill = []
+                most_sponsors_bill << bill
+            elsif the_count == most_sponsors_count
+                most_sponsors_bill << bill
+            end
+        end
+        puts "There is/are #{most_sponsors_bill.length} bill(s) with the most (#{most_sponsors_count}) sponsors."
+        return {most_sponsors_count => [most_sponsors_bill]}
+    end
+
+
+    def self.least_sponsors
+        least_sponsors_count = 500
+        least_sponsors_bill = []
+        Bill.all.each do |bill|
+            the_count = bill.sponsorships.count
+            if the_count < least_sponsors_count
+                least_sponsors_count = the_count
+                least_sponsors_bill = []
+                least_sponsors_bill << bill
+            elsif the_count == least_sponsors_count
+                least_sponsors_bill << bill
+            end
+        end
+        puts "There is/are #{least_sponsors_bill.length} bill(s) with the least (#{least_sponsors_count}) sponsors."
+        return {least_sponsors_count => [least_sponsors_bill]}
+    end
+
+
+    def self.most_dem
+        # looks through all bills and counts how many dem sponsors per bill, returns the bill with largest count
+        most_dem_count = 0
+        most_dem_bill = []
+        i = 1
+        while i <= Bill.all.length
+            the_count = Bill.find(i).sponsorships.count do |sponsor|
+                if sponsor["legislator_id"] != 0
+                    Legislator.find(sponsor["legislator_id"]).party == "Democratic"
+                end    
+            end    
+            if the_count > most_dem_count
+                most_dem_count = the_count
+                most_dem_bil = []
+                most_dem_bill << Bill.find(i)
+            elsif the_count == most_dem_count
+                most_dem_bill << Bill.find(i)
+            end    
+            i += 1
+        end    
+        titles = most_dem_bill.map {|bill| bill.title}
+        puts "There is/are #{titles.length} bill(s) with the most (#{most_dem_count}) dem sponsors: #{titles}."
+        return most_dem_bill
+    end    
+
+
+    def self.most_repub
+        #same as above with repub
+        most_repub_count = 0
+        most_repub_bill = []
+        i = 1
+        while i <= Bill.all.length
+            the_count = Bill.find(i).sponsorships.count do |sponsor|
+                if sponsor["legislator_id"] != 0
+                    Legislator.find(sponsor["legislator_id"]).party == "Republican"
+                end    
+            end    
+            if the_count > most_repub_count
+                most_repub_count = the_count
+                most_repub_bill = []
+                most_repub_bill << Bill.find(i)
+            elsif the_count == most_repub_count
+                most_repub_bill << Bill.find(i)
+            end    
+            i += 1
+        end    
+        titles = most_repub_bill.map {|bill| bill.title}
+        puts "There is/are #{titles.length} bill(s) with the most (#{most_repub_count}) repub sponsors: #{titles}."
+        return most_repub_bill
+    end        
 
 
     def bipartisan_count
         dem_count = sponsor_count_by_party("Democratic")
         repub_count = sponsor_count_by_party("Republican")
         puts "This bill had #{dem_count} dem sponsors and #{repub_count} repub sponsors."
-        return {"dem" => dem_count, "repub" => repub_count}
+        return {"Democratic" => dem_count, "Republican" => repub_count}
     end
 
 
     def self.most_bipartisan
         #iterates thru each bill to find bipartisan_count, creates a ratio, sorts, returns middle
-    
     end
 
 
@@ -113,6 +153,7 @@ class Bill < ActiveRecord::Base
         array_of_names = turn_array_of_legislators_to_full_names(array)
         puts "There are #{array_of_names.count} primary sponsors: #{array_of_names}"
         return array
+        # use eddie's method here to have D or R next to name
     end
 
 
@@ -127,13 +168,50 @@ class Bill < ActiveRecord::Base
     end
 
 
+    def all_sponsors_by_party
+        #returns a hash {dem: [instancs], repub: [instances]}
+        array_of_sponsors = self.all_sponsors
+        cleaned_array = clean_array_of_leg_id_0(array_of_sponsors)
+        dem_array = []
+        repub_array = []
+        cleaned_array.each do |sponsor|
+            if Legislator.find(sponsor["legislator_id"]).party == "Democratic"
+                dem_array << Legislator.find(sponsor["legislator_id"])
+            elsif Legislator.find(sponsor["legislator_id"]).party == "Republican"
+                repub_array << Legislator.find(sponsor["legislator_id"])
+            end
+        end
+        puts "#{dem_array.count} dems and #{repub_array.count} repubs"
+        return {"Democratic" => dem_array, "Republican" => repub_array}
+    end
+
+
     def subjects
         #output array with subjects of a bill
+        puts self["subjects"]
+        return self["subjects"]
     end
 
 
-    def self.subjects
+    # def self.subjects
         #above method on all bills and output array.uniq
-    end
+        # array = []
+        # new_array = Bill.all.each do |bill|
+        #     nunu_array = array.concat(bill.subjects)
+        # end
+        # new_array.uniq b
+    # end
+
+    ## pending ##
+    # def self.subjects
+    #     subjects_array = []
+    #     self.bills.map do | bill |
+    #         subjects_array.push(bill.subjects)
+    #     end
+    #     clean_array = subjects_array.map do |item|
+    #         item.gsub(/[\\"+\[+\]]/,"").to_s.split(", ")
+    #     end
+    #     clean_array.flatten.uniq
+    # end
 
 end
